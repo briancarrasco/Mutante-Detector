5d1b77d7714aa851a8a0248a6eb5a4630390838c
briancarrasco <briancarrasco000@gmail.com>
2024-11-02 19:53:30 -0300

    Add files via upload

---
+++ mutante-detector-main/mutantedetector/src/main/java/com/example/mutante_detector/controller/MutantController.java
@@ -0,0 +1,33 @@
+package com.example.mutante_detector.controller;
+
+import com.example.mutante_detector.service.MutantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/mutant")
+public class MutantController {
+
+    private final MutantService mutantService;
+
+    @Autowired
+    public MutantController(MutantService mutantService) {
+        this.mutantService = mutantService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> isMutant(@RequestBody Map<String, String[]> dnaRequest) {
+        String[] dna = dnaRequest.get("dna");
+        if (mutantService.isMutant(dna)) {
+            return ResponseEntity.ok(Map.of("mensaje", "Mutante detectado"));
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body(Map.of("mensaje", "No un mutante"));
+        }
+    }
+
+}
